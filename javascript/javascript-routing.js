dfdddab138f65af9a0bfa0e4d21ac35aa0b58952
Ammar <ammarahmed4517331@gmail.com>
2017-03-21 16:35:05 +0500

    Change route

--- javascript/javascript-routing.js
+++ javascript/javascript-routing.js
@@ -1,3 +1,6 @@
 /**
  * Created by Ammar on 3/20/2017.
  */
+function goForward() {
+    window.history.forward()
+}
---
+++ javascript/pagination.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ammar on 3/21/2017.
+ */
+
+function loadContact1(){
+    history.pushState({},"","vertu/1");
+}
+function loadProduct2(){
+    history.pushState({},"","vertu/2");
+}
+function loadProduct3(){
+    history.pushState({},"","vertu/3");
+}
+function loadProduct4(){
+    history.pushState({},"","vertu/4");
+}
+function loadProduct5(){
+    history.pushState({},"","vertu/5");
+}
