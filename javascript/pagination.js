3f6c5dc44cc8208a495428470b42a47fd910b86b
Ammar <ammarahmed4517331@gmail.com>
2017-03-25 16:07:41 +0500

    add routing img

--- javascript/pagination.js
+++ javascript/pagination.js
@@ -1,19 +1,30 @@
 /**
  * Created by Ammar on 3/21/2017.
  */
-
 function loadContact1(){
-    history.pushState({},"","vertu/1");
+    history.pushState({page:3},"page 1","/routing/1");
+
+    document.getElementById("image-1").setAttribute("src", "images/Star_Guy_01@2x.png");
 }
 function loadProduct2(){
-    history.pushState({},"","vertu/2");
+    //history.replaceState({},"routing/1","routing/2");
+    history.pushState({page:3},"page 1","/routing/2");
+    document.getElementById("image-1").setAttribute("src", "images/Star_Guy_02@2x.png");
 }
-function loadProduct3(){
-    history.pushState({},"","vertu/3");
+function loadProduct3() {
+    //history.pushState({}, "", "routing/3");
+    history.pushState({page:3},"page 1","/routing/3");
+    document.getElementById("image-1").setAttribute("src", "images/a01.png");
+    //if(history.pushState({}, "", "routing/3")){}
+    //else{}
 }
 function loadProduct4(){
-    history.pushState({},"","vertu/4");
+    //history.pushState({},"","routing/4");
+    document.getElementById("image-1").setAttribute("src", "images/a02.png");
+    history.pushState({page:3},"page 1","/routing/4");
 }
 function loadProduct5(){
-    history.pushState({},"","vertu/5");
+    //history.pushState({},"","routing/5");
+    document.getElementById("image-1").setAttribute("src", "images/a03.png");
+    history.pushState({page:3},"page 1","/routing/5");
 }
